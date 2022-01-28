90465b6e19ef414efb3bf82738bd6206b74a049b
reddi32 <rossimatteo32@gmail.com>
2022-01-28 19:04:05 +0100

    Update

--- app/src/main/java/com/redlabpc/timeforpercentage/MainActivity.kt
+++ app/src/main/java/com/redlabpc/timeforpercentage/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.redlabpc.timeforpercentage
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.redlabpc.timeforpercentage.databinding.ActivityMainBinding
 import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,41 +16,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateButton.setOnClickListener{ calculateTip() }
+
     }
 
-    fun calculateTip() {
+    @SuppressLint("ResourceType")
+    private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField2 = binding.percentuale.text.toString()
         val cost = stringInTextField.toDouble()
-        val selectId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectId) {
-            R.id.option_novantacinque -> 0.95
-            R.id.option_novanta -> 0.90
-            R.id.option_ottantacinque -> 0.85
-            R.id.option_ottanta -> 0.80
-            R.id.option_settantacinque -> 0.75
-            R.id.option_settanta -> 0.70
-            R.id.option_sessantacinque -> 0.65
-            R.id.option_sessanta -> 0.60
-            R.id.option_cinquantacinque -> 0.55
-            R.id.option_cinquanta -> 0.50
-            R.id.option_quarantacinque -> 0.45
-            R.id.option_quaranta -> 0.40
-            R.id.option_trentacinque -> 0.35
-            R.id.option_trenta -> 0.30
-            R.id.option_venticinque -> 0.25
-            R.id.option_venti -> 0.20
-            R.id.option_quindici -> 0.15
-            R.id.option_dieci -> 0.10
-            R.id.option_cinque -> 0.05
-            else -> 0.05
-        }
+        val costsecond = stringInTextField2.toDouble()
 
-        var tip = tipPercentage * cost
-        val roundUp = binding.roundUpSwitch.isChecked
-        if (roundUp) {
-            tip = kotlin.math.ceil(tip)
-        }
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        var calcolopercentuale = cost*costsecond/100
+        var result = cost - calcolopercentuale
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(calcolopercentuale)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+        val resultTip = NumberFormat.getCurrencyInstance().format(result)
+        binding.tipFinal.text = getString(R.string.tip_final, resultTip)
     }
 }
+
